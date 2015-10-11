9fd0d2c5111a7f2455c327f8bde052fae8d32eb7
Hideaki Nagamine <hnagamin@kmc.gr.jp>
2015-10-11 18:13:38 +0900

    use OCaml

---
+++ src/operation.ml
@@ -0,0 +1,30 @@
+type operand =
+  Push
+| Pop
+| Dup
+| Roll
+| Add
+| Mul
+| Sub
+| Div
+| Greater
+| Not
+| Mod
+| Ifte
+| Jez
+| Jmp
+| Label
+| Gosub
+| Return
+| Inn
+| Inc
+| Outn
+| Outc
+| Nop
+| Halt
+| Malloc
+| Loadh
+| Storeh
+| Loads
+| Stores
+
